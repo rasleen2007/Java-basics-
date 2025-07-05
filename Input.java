2737bcb03b91dbfd9ee79cff1688d4d09f59afe2
rasleen2007 <83662684+rasleen2007@users.noreply.github.com>
2025-07-05 20:42:56 +0530

    Add files via upload
    EXPLANATION:

---
+++ Input.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Input{
+        public static void main(String[] args){
+            //Input
+            Scanner sc= new Scanner(System.in);
+
+            String name=sc.nextLine();
+            System.out.println(name);
+            sc.close();
+        }
+}
+    
+
